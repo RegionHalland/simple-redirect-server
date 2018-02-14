9e04b010667f539132751aa5654aca54f637f186
Sebastian Marcusson <sebastian.marcusson@gmail.com>
2018-02-14 15:17:03 +0100

    Add update example

--- example/index.js
+++ example/index.js
@@ -4,5 +4,5 @@ var srs = require("../index");
 srs.init({
     portListen: 1337, //HTTP server will listen on this
     httpStatusCode: 301, //For example 301,
-    csvFile: "./redirects.csv"
+    csvFile: __dirname + "/redirects.csv" //Path to csv file used for redirections
 });
